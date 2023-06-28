1ba23f3138bed83674b2b15e85cd49ab1cca74d0
Daniel Shapero <shapero.daniel@gmail.com>
2023-06-28 14:15:42 -0700

    Add flow around cylinder plot

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 
 tensor-product-stokes.pdf: tensor-product-stokes.tex tensor-product-stokes.bib
+	cd demo/flow-around-cylinder && make cylinder_flow.pdf && cd ../../
 	pdflatex tensor-product-stokes.tex
 	bibtex tensor-product-stokes
 	pdflatex tensor-product-stokes.tex
--- demo/flow-around-cylinder/make_plots.py
+++ demo/flow-around-cylinder/make_plots.py
@@ -10,32 +10,40 @@ args = parser.parse_args()
 with firedrake.CheckpointFile(args.input, "r") as chk:
     mesh = chk.load_mesh("mesh")
     u = chk.load_function(mesh, name="velocity")
-    p = chk.load_function(mesh, name="pressure")
+
+# Get the horizontal element from the velocity space and create spaces both on
+# the extruded space and the base mesh for the depth-averaged velocity
+V = u.function_space()
+elt_x = V.ufl_element().sub_elements()[0].sub_elements()[0]
+r = firedrake.FiniteElement("R", "interval", 0)
+avg_velocity_element = firedrake.TensorProductElement(elt_x, r)
+V_avg = firedrake.VectorFunctionSpace(mesh, avg_velocity_element, dim=2)
 
 base_mesh = mesh._base_mesh
-
-degree = 1
-cg_x = firedrake.FiniteElement("CG", "triangle", degree)
-b_x = firedrake.FiniteElement("Bubble", "triangle", degree + 2)
+V_2 = firedrake.VectorFunctionSpace(base_mesh, elt_x)
 
 # Compute the depth-averaged horizontal velocity field and plot it.
-r = firedrake.FiniteElement("R", "interval", 0)
-avg_pressure_element = firedrake.TensorProductElement(cg_x, r)
-Q_avg = firedrake.FunctionSpace(mesh, avg_pressure_element)
-p_avg = firedrake.project(p, Q_avg)
-Q_2 = firedrake.FunctionSpace(base_mesh, cg_x)
-p_2 = firedrake.Function(Q_2)
-p_2.dat.data[:] = p_avg.dat.data_ro[:]
-
-avg_velocity_element = firedrake.TensorProductElement(cg_x, r)
-V_avg = firedrake.VectorFunctionSpace(mesh, avg_velocity_element, dim=2)
 u_avg = firedrake.project(firedrake.as_vector((u[0], u[1])), V_avg)
-V_2 = firedrake.VectorFunctionSpace(base_mesh, cg_x)
 u_2 = firedrake.Function(V_2)
 u_2.dat.data[:] = u_avg.dat.data_ro[:]
 
-fig, ax = plt.subplots()
-ax.set_aspect("equal")
-colors = firedrake.quiver(u_2, axes=ax, headwidth=1)
-fig.colorbar(colors)
+fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
+for ax in axes:
+    ax.set_aspect("equal")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+kw = {
+    "interior_kw": {"linewidth": 0.25},
+    "boundary_kw": {
+        "linewidth": 2,
+        "colors": [
+            "tab:blue", "tab:orange", "tab:green", "tab:purple", "tab:grey"
+        ],
+    },
+}
+firedrake.triplot(base_mesh, axes=axes[0], **kw)
+axes[0].legend()
+firedrake.quiver(u_2, axes=axes[1], headwidth=2)
+
 fig.savefig(args.output, bbox_inches="tight")
